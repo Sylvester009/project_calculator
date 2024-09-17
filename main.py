2bb064a971e66dbffe253ed7e50f8ae759b292dd
Sylvester009 <sasy260902@gmail.com>
2024-09-17 07:45:58 +0100

    more setup test for project

--- main.py
+++ main.py
@@ -3,10 +3,14 @@ from tkinter import *
 root = Tk()
 
 mylabel = Label(root, text="Hello world")
-mylabel2 = Label(root, text="Hello world")
+mylabel2 = Label(root, text="Hello world!")
 
+mybutton = Button(root, text="close", fg="white", bg="pink")
+
+
+mybutton.grid(row=1, column=1)
 mylabel.grid(row=0, column=0)
-mylabel2.grid(row=1, column=1)
+mylabel2.grid(row=2, column=2)
 
 
 root.mainloop()
